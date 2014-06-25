535cc67144ddc3180de68f4b6aad04433b241618
Taylor Buley <taylor.buley@gmail.com>
2014-06-25 17:25:59 -0400

    gtalk

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 export HUBOT_XMPP_USERNAME="buleyllc@gmail.com"
+export HUBOT_GTALK_USERNAME="buleyllc@gmail.com"
 export HUBOT_XMPP_PASSWORD="tb7749!@"
+export HUBOT_GTALK_PASSWORD="tb7749!@"
 export HUBOT_XMPP_ROOMS="buley@conference.jabber.org"
 export HUBOT_XMPP_HOST="talk.google.com"
 export HUBOT_XMPP_PORT="5222"
 export REDISTOGO_URL="redis://redistogo:701efba59ca8088d6ff4ca0666418877@beardfish.redistogo.com:9182"
-./bin/hubot -a xmpp -n buley
+./bin/hubot -a gtalk -n buley
