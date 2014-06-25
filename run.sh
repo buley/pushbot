a6a25ae2aec679ccea055d078792a7493fb14a66
Taylor Buley <taylor.buley@gmail.com>
2014-06-25 13:50:08 -0400

    WIP

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export HUBOT_XMPP_USERNAME="buleyllc@gmail.com"
+export HUBOT_XMPP_PASSWORD="tb7749!@"
+export HUBOT_XMPP_ROOMS="buley@conference.jabber.org"
+export HUBOT_XMPP_HOST="talk.google.com"
+export HUBOT_XMPP_PORT="5222"
+export REDISTOGO_URL="redis://redistogo:701efba59ca8088d6ff4ca0666418877@beardfish.redistogo.com:9182"
+./bin/hubot -a xmpp -n buley
